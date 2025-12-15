b9ea1265121484a4fa19b718c9c5611c2826785f
Usamah-Abdul-Aziz <ikmal.usamah@gmail.com>
2025-12-15 18:07:46 +0700

    emailconfig.1

--- lib/config/supabase_config.dart
+++ lib/config/supabase_config.dart
@@ -2,4 +2,6 @@ class SupabaseConfig {
   // Ganti dengan URL dan Anon Key Supabase Anda
   static const String supabaseUrl = 'https://rynlfumkxecgngslxdwr.supabase.co';
   static const String supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJ5bmxmdW1reGVjZ25nc2x4ZHdyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjU3NjUzMzgsImV4cCI6MjA4MTM0MTMzOH0.Yo7zyAsCIRgBvXcWQ9G8a69ELceaHXyvMlqMFF6lWQk';
+  // Resend Api Key = re_NS5ahFnC_9SN89ZJm6ruthZDYxLGPVMnZ
 }
+
